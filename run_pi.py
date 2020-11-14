ad6d0075b5a1e30c4b189a0cbeddf79a33251916
TomPretty <tompretty2@gmail.com>
2020-11-14 19:39:13 +0000

    Add run_pi.py

---
+++ run_pi.py
@@ -0,0 +1,50 @@
+import os
+import time
+
+from dotenv import load_dotenv
+
+from api import CachedApi, LoggedApi, OpenWeatherApi
+from data import (
+    OpenWeatherSunsetDataSource,
+    OpenWeatherTempDataSource,
+    OpenWeatherWeatherDataSource,
+)
+from log import CompositeLogger, ConsoleLogger, EmailLogger
+from ui.displays import WaveshareEpdDisplay
+from ui.widgets import CurrentWeatherWidget, SunsetWidget, TabbedWidget
+from ui.windows import PilWindow
+
+load_dotenv()
+
+UI_UPDATE_INTERVAL = int(os.getenv("UI_UPDATE_INTERVAL"))
+API_CACHE_TIME = int(os.getenv("API_CACHE_TIME"))
+
+open_weather_api = CachedApi(
+    api=LoggedApi(
+        api=OpenWeatherApi(),
+        logger=CompositeLogger(loggers=[ConsoleLogger(), EmailLogger()]),
+    ),
+    cache_time_in_seconds=API_CACHE_TIME,
+)
+
+window = PilWindow(width=264, height=176)
+widget = TabbedWidget(
+    children=[
+        CurrentWeatherWidget(
+            temperature_data_source=OpenWeatherTempDataSource(api=open_weather_api),
+            weather_data_source=OpenWeatherWeatherDataSource(api=open_weather_api),
+        ),
+        SunsetWidget(data_source=OpenWeatherSunsetDataSource(api=open_weather_api)),
+    ]
+)
+display = WaveshareEpdDisplay()
+display.init()
+
+
+while True:
+    window.clear()
+
+    widget.draw(window)
+    display.draw(window)
+
+    time.sleep(UI_UPDATE_INTERVAL)
