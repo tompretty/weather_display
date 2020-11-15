b9521eb9718c5a0a1f4ff0ab5152a79026ac4331
TomPretty <tompretty2@gmail.com>
2020-11-15 21:54:46 +0000

    Add types + docstrings to ui.windows

--- ui/windows/base.py
+++ ui/windows/base.py
@@ -1,30 +1,111 @@
 from abc import ABC, abstractmethod
+from typing import Tuple
+
+from PIL import Image  # type: ignore
 
 
 class Window(ABC):
-    def __init__(self, width, height):
+    """Window - A base class for windows.
+
+    A window is responsible for rendering an image, which is then displayed by
+    a `Display`. A window provides a number of methods for adding graphical elements
+    to the image e.g text and images.
+
+    Parameters
+    ----------
+    width : int
+        The width of the image
+    height: int
+        The height of the image
+    """
+
+    def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
 
-    def clear(self):
+    def clear(self) -> None:
+        """Clear the current image.
+        """
         pass
 
     @abstractmethod
-    def rectangle(self, left, top, right, bottom):
+    def rectangle(self, left: int, top: int, right: int, bottom: int) -> None:
+        """Draw a rectangle onto the screen.
+
+        Parameters
+        ----------
+        left : int
+            x co-ord of the left edge
+        top : int
+            y co-ord of the top edge
+        right : int
+            x co-ord of the right edge
+        bottom : int
+            y co-ord of teh bottom edge
+        """
         raise NotImplementedError
 
     @abstractmethod
-    def text(self, x, y, body, font_path, font_size):
+    def text(self, x: int, y: int, body: str, font_path: str, font_size: int):
+        """Draw some text to the screen
+
+        Parameters
+        ----------
+        x : int
+            x co-ord of start of text
+        y : int
+            y co-ord of start of text
+        body : str
+            the text to draw
+        font_path : str
+            path to the font file
+        font_size : int
+            font size to draw
+        """
         raise NotImplementedError
 
     @abstractmethod
-    def text_size(self, body, font_path, font_size):
+    def text_size(self, body: str, font_path: str, font_size: int) -> Tuple[int, int]:
+        """Get size of the texts bounding box.
+
+        Parameters
+        ----------
+        body : str
+            the text
+        font_path : str
+            path to font file
+        font_size : int
+            font size
+
+        Returns
+        ------
+        Tuple[int, int]
+            width and height of the bounding box
+        """
         raise NotImplementedError
 
     @abstractmethod
-    def image(self, x, y, path):
+    def image(self, x: int, y: int, path: str) -> None:
+        """Draw an image to the screen.
+
+        Parameters
+        ----------
+        x : int
+            x co-ord of start of image
+        y : int
+            y co-ord of start of image
+        path : str
+            path to image file
+        """
         raise NotImplementedError
 
     @abstractmethod
-    def get_pil_image(self):
+    def get_pil_image(self) -> Image:
+        """Get the rendered image as a PIL image.
+
+        Returns
+        -------
+        Image
+            the rendered image
+        """
         raise NotImplementedError
--- ui/windows/pil_window.py
+++ ui/windows/pil_window.py
@@ -1,33 +1,45 @@
-from PIL import Image, ImageDraw, ImageFont
+from typing import Tuple
+
+from PIL import Image, ImageDraw, ImageFont  # type: ignore
 
 from .base import Window
 
 
 class PilWindow(Window):
-    def __init__(self, width, height):
+    """Pil window - Renders an image using the PIL library
+
+    Parameters
+    ----------
+    width : int
+        the width of the window
+    height : int
+        the height of the window
+    """
+
+    def __init__(self, width: int, height: int) -> None:
         super().__init__(width, height)
         self._image = Image.new(mode="1", size=(width, height), color=1)
 
     def clear(self):
         self._image = Image.new(mode="1", size=self._image.size, color=1)
 
-    def rectangle(self, left, top, right, bottom):
+    def rectangle(self, left: int, top: int, right: int, bottom: int) -> None:
         draw = ImageDraw.Draw(self._image)
         draw.rectangle([left, top, right, bottom], fill=0)
 
-    def text(self, x, y, body, font_path, font_size):
+    def text(self, x: int, y: int, body: str, font_path: str, font_size: int) -> None:
         font = ImageFont.truetype(font_path, font_size)
         draw = ImageDraw.Draw(self._image)
         draw.text((x, y), body, font=font)
 
-    def text_size(self, body, font_path, font_size):
+    def text_size(self, body: str, font_path: str, font_size: int) -> Tuple[int, int]:
         font = ImageFont.truetype(font_path, font_size)
         draw = ImageDraw.Draw(self._image)
         return draw.textsize(body, font=font)
 
-    def image(self, x, y, path):
+    def image(self, x: int, y: int, path: str):
         im = Image.open(path)
         self._image.paste(im, box=(x, y))
 
-    def get_pil_image(self):
+    def get_pil_image(self) -> Image:
         return self._image
